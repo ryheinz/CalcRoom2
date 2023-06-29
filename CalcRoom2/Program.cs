c8c8c5e986014a6aff91d8c54478ad43b6e9d57f
ryheinz <ryan.heinz@gmail.com>
2023-06-29 21:16:17 +0200

    working on if/else--not finished

--- CalcRoom2/Program.cs
+++ CalcRoom2/Program.cs
@@ -3,7 +3,8 @@
 
 using System;
 
-namespace CalcRoom2  
+
+namespace CalcRoom2
 {
     class CalcRoom
     {
@@ -11,44 +12,50 @@ namespace CalcRoom2
         //user question to get room shape
 
         {
-            Console.Write("Do you want to get cost for a square room? Plese enter Y for Yes or N for No");
+            Console.WriteLine("Welcome to the Room CALC2! ");
+            Console.Write("Do you want to get cost for a square room? Please enter Y for Yes or N for No. ");
+            Console.ReadLine();
+        }
+        bool answerY = true;
+        bool answerN = false;
+
+        If(answerY = Y)// Square room:
+
+        {
+            Console.Write("Enter length of room in feet: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter width of room in feet:");
+            double width = Convert.ToDouble(Console.ReadLine());
+            double area = length * width;
+
+            Console.Write("Enter price of square feet: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+            double cost = (length * width) * price;
+
+            Console.WriteLine($"Floor is {area} square feet.");
+            Console.WriteLine($"Cost for room is {cost} .");
+            Console.ReadLine();
 
         }
-            
-            
-            
-            // Square room:
-        //{
-        //    Console.Write("Enter length of room in feet: ");
-        //    double length = Convert.ToDouble(Console.ReadLine());
-        //    Console.Write("Enter width of room in feet:");
-        //    double width = Convert.ToDouble(Console.ReadLine());
-        //    double area = length * width;
-        //    Console.Write("Enter price of square feet: ");
-        //    double price = Convert.ToDouble(Console.ReadLine());
-        //    double cost = (length * width) * price;
-        //    Console.WriteLine($"Floor is {area} square feet.");
-        //    Console.WriteLine($"Cost for room is {cost} .");
-        //    Console.ReadLine();
-        //}
+        Else(answerN)
         // Circular room:
-        //{
-        //    Console.Write("Enter diameter of room in feet: ");
-        //    double diameter = Convert.ToDouble(Console.ReadLine());
-        //    double radius = (diameter / 2);
-        //    double area = Math.Round(radius * Math.PI,2);
+        {
+            Console.Write("Enter diameter of room in feet: ");
+            double diameter = Convert.ToDouble(Console.ReadLine());
+            double radius = (diameter / 2);
+            double area = Math.Round(radius * Math.PI, 2);
 
-        //    Console.Write("Enter price of square feet: ");
-        //    double price = Convert.ToDouble(Console.ReadLine());
-        //    double cost = area * price;
+            Console.Write("Enter price of square feet: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+            double cost = area * price;
 
-        //    Console.WriteLine($"Floor is {area} square feet.");
-        //    Console.WriteLine($"Cost for room is {cost}.");
-        //    Console.ReadLine();
+            Console.WriteLine($"Floor is {area} square feet.");
+            Console.WriteLine($"Cost for room is {cost}.");
+            Console.ReadLine();
         }
         //average flooring team can only put in 20 square feet of flooring per hour at a cost of $86.00/hr
+
     }
-
-
 
 }
